7d5a4f8f2e95f69d2896689e2da834a6ca716175
Chorder <test@bookworm>
2024-12-27 22:33:42 +0800

    add download links

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -2,8 +2,11 @@
     <div class="container">
       <h1>下载</h1>
       <p>点击下方链接进入插件商店安装插件</p>
-      <a href="https://example.com/download" class="">Firefox插件地址</a><br/>
-      <a href="https://example.com/download" class="">Chrome插件地址</a><br/>
+      <a href="https://github.com/Chorder/danmuju/tree/master/clients/danmuju-firefox" class="">Firefox版本源码地址</a><br/>
+      <a href="https://github.com/Chorder/danmuju/tree/master/clients/danmuju-chrome" class="">Chrome版本源码地址</a><br/>
+
+      <a href="https://github.com/Chorder/danmuju/tree/master/clients" class="">插件压缩包下载地址</a><br/>
+
     </div>
   </template>
   
